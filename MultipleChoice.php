86cb0d3db8eea5e7d72f0b686c0ddea22ea3ec93
Omri Ben Ishai <omribenishai015@gmail.com>
2017-08-23 22:42:27 +0300

    Project Files

---
+++ MultipleChoice.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+
+        </style>    
+    <body>
+        <?php
+        define("x", "5", TRUE);
+        $x = x + 10;
+        echo X;
+        ?>
+        
+        <?php
+        $arr = array(5 => 1, 122 => 2);
+        $arr["x"] = 42;    
+        $arr[] = 56;
+        echo var_dump($arr);
+        ?>
+
+        <?php 
+            var="hello";
+            $a = 1;
+            $b = 2;
+            print ("<html><body><ul><li>$a and $b</li></ul></body></html>");
+        ?>
+    </body>
+
+</head>
+
